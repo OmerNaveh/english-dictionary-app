2c0d2ef18b2b272809d6ea677036d002c1f4946d
Omer Naveh <89572395+OmerNaveh@users.noreply.github.com>
2022-01-19 19:53:54 +0200

    secondary indexes needs revisiting

--- back/controllers/getRandomWordBySpeechPat.js
+++ back/controllers/getRandomWordBySpeechPat.js
@@ -4,15 +4,12 @@ const getRandomWordBySpeechPart = async (req, res, next) => {
   const { pos } = req.params;
   let { letter } = req.query;
   if (!pos) return next("missing params");
-  const expression = letter
-    ? "pos=:pos AND begins_with (word , :letter)"
-    : "pos=:pos";
   if (!letter) {
     letter = "";
   }
   const params = {
     TableName: "Dictionary",
-    KeyConditionExpression: expression,
+    KeyConditionExpression: "pos=:pos AND begins_with (word , :letter)",
     ExpressionAttributeValues: {
       ":letter": letter.toUpperCase(),
       ":pos": pos,
@@ -27,22 +24,22 @@ const randNum = () => {
   return Math.ceil(Math.random() * 5);
 };
 
-const check = async (pos, letter) => {
-  if (!letter) {
-    letter = "";
-  }
-  const params = {
-    TableName: "Dictionary",
-    KeyConditionExpression: "begins_with(word , :letter) AND pos=:pos",
-    ExpressionAttributeValues: {
-      ":letter": letter.toUpperCase(),
-      ":pos": pos,
-    },
-    Limit: 5,
-  };
-  const val = await ddb.query(params).promise();
-  if (!val.Items) next("unrecognizable word");
-  console.log(val.Items[randNum]);
-};
-check("n.", "v");
+// const check = async (pos, letter) => {
+//   if (!letter) {
+//     letter = "";
+//   }
+//   const params = {
+//     TableName: "Dictionary",
+//     KeyConditionExpression: "begins_with(word , :letter) AND pos=:pos",
+//     ExpressionAttributeValues: {
+//       ":letter": letter.toUpperCase(),
+//       ":pos": pos,
+//     },
+//     Limit: 5,
+//   };
+//   const val = await ddb.query(params).promise();
+//   if (!val.Items) next("unrecognizable word");
+//   console.log(val.Items[randNum]);
+// };
+// check("n.", "v");
 module.exports = { getRandomWordBySpeechPart };
