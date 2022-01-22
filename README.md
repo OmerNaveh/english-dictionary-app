9472c5107ebddc6c7277ae7270aff38ee2777e2a
Omer Naveh <89572395+OmerNaveh@users.noreply.github.com>
2022-01-22 13:19:37 +0200

    finished locally

--- README.md
+++ README.md
@@ -2,11 +2,12 @@
 
 ### Using this app you can search by any word or by word and part of speech to recieve a descriptive output <br/> for over **107,000 words** in the english language
 
----
+<br/><img src="./assets/homepage.jpg" alt="homeimg" width=200px heigh=200px>
 
 ## This app uses multiple technologies and libraries to provide an english dictionary
 
 - **React JS**
+- **Serverless framework**
 - **AWS-SDK**
 - **Express**
 - **Axios**
--- back/index.js
+++ back/index.js
@@ -1,24 +1,30 @@
 const express = require("express");
+// const serverless = require("aws-serverless-express");
 const cors = require("cors");
-require("dotenv").config();
 const { getByWord } = require("./controllers/getByWord");
 const {
   getByWordAndSpeechType,
 } = require("./controllers/getByWordAndSpeechType");
-const {
-  getRandomWordBySpeechPart,
-} = require("./controllers/getRandomWordBySpeechPat");
+// const {
+//   getRandomWordBySpeechPart,
+// } = require("./controllers/getRandomWordBySpeechPat");
 const errorHandler = require("./middlewares/errorHandler");
 
 const app = express();
-const port = process.env.PORT || 4000;
+
 app.use(cors());
 
 app.get("/:word", getByWord);
 app.get("/:word/:partOfSpeech", getByWordAndSpeechType);
-app.get("/part-of-speech/:part", getRandomWordBySpeechPart);
+// app.get("/part-of-speech/:part", getRandomWordBySpeechPart);
 
 app.use(errorHandler);
+
+// const server = serverless.createServer(app);
+// module.exports.handler = (event, context) => {
+//   serverless.proxy(server, event, context);
+// };
+const port = 4000;
 app.listen(port, () => {
   console.log(`running on ${port}`);
 });
